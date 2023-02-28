6fda7272baf3dd600d5af1d3c1ad602e8578ee95
MartinezMG10 <garcia.miguelangel1206@gmail.com>
2023-02-28 13:51:07 -0500

    version funcional

--- main.js
+++ main.js
@@ -6,60 +6,85 @@ const BotonMultiplicacion = document.getElementById("btn_*")
 const BotonIgual = document.getElementById("btn_=")
 const Simbolo = document.getElementById("label")
 const AC = document.getElementById("btn_AC")
+const num1 = document.getElementById("num1")
 
 
 function operador(operando) {
     switch (operando) {
         case '+':
-          resultado=parseFloat(num1.value)+ parseFloat(num2.value)
+          resultado=parseFloat(numero1)+ parseFloat(num2)
           console.log(resultado)
           break;
         case '-':
-            resultado=parseFloat(num1.value)- parseFloat(num2.value)
+            resultado=parseFloat(numero1)- parseFloat(num2)
             console.log(resultado)
           break;
         case '/':
-            resultado=parseFloat(num1.value)/ parseFloat(num2.value)
+            resultado=parseFloat(numero1)/ parseFloat(num2)
             console.log(resultado)
           break;
         case '*':
-            resultado=parseFloat(num1.value)*parseFloat(num2.value)
+            resultado=parseFloat(numero1)*parseFloat(num2)
             console.log(resultado)
           break;
       }
     }
+
+    
 BotonDivision.addEventListener("click", () => {
     Simbolo.innerText="/"
+    numero1= num1.value
+    num1.value = " "
+
     BotonIgual.addEventListener("click",() => {
+        num2 = num1.value
         operador("/")
         Display.value=resultado
     })
+
+    
 })
 
 BotonSuma.addEventListener("click", () => {
     Simbolo.innerText="+"
+    numero1= num1.value
+    num1.value = " "
+    
+
 
     BotonIgual.addEventListener("click",() => {
+        num2 = num1.value
         operador("+")
         Display.value=resultado
+
     })
+    
 })
 
 BotonResta.addEventListener("click", () => {
     Simbolo.innerText="-"
+    numero1= num1.value
+    num1.value = " "
 
     BotonIgual.addEventListener("click",() => {
+        num2 = num1.value
         operador("-")
         Display.value=resultado
+
     })
+
 })
 
 BotonMultiplicacion.addEventListener("click", () => {
     Simbolo.innerText="*"
+    numero1= num1.value
+    num1.value = " "
 
     BotonIgual.addEventListener("click",() => {
+        num2 = num1.value
         operador("*")
         Display.value=resultado
+
     })
 })
 
@@ -68,5 +93,6 @@ AC.addEventListener("click", () => {
     Simbolo.innerText=" "
     Display.value = " "
     num1.value = " "
-    num2.value = " "
+    numero1 =0
+    num2 = 0
 })
